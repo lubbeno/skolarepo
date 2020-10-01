5b17e1fb0ad0bf30c073ea588109224755f1a578
Lubomír Sukup <lubomir_s@sosholic3.local>
2020-10-01 09:54:04 +0200

    skupinaB uloha zadanie

--- src/lubos/sukup/main/skupinaB/Main.java
+++ src/lubos/sukup/main/skupinaB/Main.java
@@ -1,9 +1,27 @@
 package lubos.sukup.main.skupinaB;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+       // System.in.read();
+       // System.err.println();
+        System.out.println("mapist text: ");
+        Scanner scanner = new Scanner(System.in);
+
+        String out = scanner.nextLine();
+        int pocet = pocetZnakov(out);
+        //vytvorte metodu ktora spocita pocet znakov v zadamom texte
+        System.out.println("vystupny text: "+out);
+        System.out.println("pocet znakov je: "+ pocet);
+
     }
+
+
+    // vstupna premenna metody bude String a vystup bude int
 
 }
