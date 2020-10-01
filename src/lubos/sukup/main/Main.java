7390cc91d66202ab815a7c8b7456691a12884db5
Lubomír Sukup <lubomir_s@sosholic3.local>
2020-10-01 11:50:51 +0200

    ukazka fungovania  gitu

--- src/lubos/sukup/main/Main.java
+++ src/lubos/sukup/main/Main.java
@@ -9,7 +9,6 @@ public class Main {
     }
     public static void main(String[] args) {
         System.out.println("hello world");
-
         Scanner scanner = new Scanner(System.in);
         System.out.print("napis text a stlac enter: ");
         String out = scanner.nextLine();
@@ -19,19 +18,16 @@ public class Main {
                 // treba si zapisat samohlasku a
                 // treba zapisat pocet samohlasiek
 
-        StringBuilder vystup = new StringBuilder();
-        char[] polesamohlasok = new char[]{'a','e','i','o','u'};
-            for (int x=0; x<out.length();x++){
-                for (char c : polesamohlasok) {
-                    if(out.charAt(x)==c){
-                        vystup.append(c);
-                    }
-                }
-            }
+        String vystup= vypocetSamohlasok(out);
         System.out.println("vystup z klavesnice: " +  out);
-        System.out.println(vystup.toString() + " pocet "+ vystup.length());
+        System.out.println(vystup + " pocet "+ vystup.length());
 
+         //   git init
+        //    git
 
+    }
+
+    private static String vypocetSamohlasok(String out) {
 
     }
 
@@ -48,3 +44,29 @@ public class Main {
 4. - poslat si novy projekt na github
 5. - poslat link na github repo mne na
         skolsky email*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+    StringBuilder vystup = new StringBuilder();
+    char[] polesamohlasok = new char[]{'a','e','i','o','u'};
+            for (int x=0; x<out.length();x++){
+        for (char c : polesamohlasok) {
+        if(out.charAt(x)==c){
+        vystup.append(c);
+        }
+        }
+        }*/
