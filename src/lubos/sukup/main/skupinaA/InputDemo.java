291719a6e3625af43fde8a4e5902ef8904577125
lubos <lubos@skola.com>
2020-10-15 11:07:59 +0200

    skupinaA filewriter

--- src/lubos/sukup/main/skupinaA/InputDemo.java
+++ src/lubos/sukup/main/skupinaA/InputDemo.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class IODemo {
+public class InputDemo {
 
     public static void main(String[] args) {
         FileReader reader =
---
+++ src/lubos/sukup/main/skupinaA/OutputDemoA.java
@@ -0,0 +1,27 @@
+package lubos.sukup.main.skupinaA;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class OutputDemoA {
+
+    public static void main(String[] args) {
+
+        try {
+
+       /*     // prepisnie celeho suboru
+            FileWriter fileWriter = new FileWriter("test");
+            fileWriter.write("toto je dalsia cast textu");
+            fileWriter.close();*/
+
+
+            FileWriter fileWriter = new FileWriter("test",true);
+            fileWriter.write("\ntoto je dalsia cast textu");
+            fileWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
